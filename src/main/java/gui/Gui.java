c1dd35d453e237805d1f889c247e4f8c867ce6ee
KimRyungHwan <luniawar@nate.com>
2014-12-18 06:23:36 -0500

    createMenu을 삭제했다.

--- src/main/java/gui/Gui.java
+++ src/main/java/gui/Gui.java
@@ -18,33 +18,16 @@ public class Gui extends Frame {
     }
 
     private void init() {
-        setSize(600, 600);
         setVisible(true);
         setLayout(null);
-
-        //   createMenu();
 
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
-                super.windowClosing(e);
-                dispose();
-                System.exit(0);
+
+                Panel panel = new Buliding("1b", 8, 20, 70);
+                add(panel);
             }
         });
-
-        Panel panel = new Buliding("1b", 8, 20, 70);
-        add(panel);
-    }
-
-    public void createMenu() {
-        MenuBar menuBar = new MenuBar();
-        Menu settingMenu = new Menu("환경설정");
-        settingMenu.add("설정");
-
-        menuBar.add(settingMenu);
-
-        setMenuBar(menuBar);
-
     }
 }
